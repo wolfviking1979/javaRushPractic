9acb96b9965814bd569852a7bd0c92349bcd6604
Yuriy Perezhogin <wolfviking086@gmail.com>
2025-05-10 09:51:48 +0500

    add Task0807, Task0806

---
+++ Core/Math/Task0806/Main.java
@@ -0,0 +1,21 @@
+package Math.Task0806;
+
+//import java.io.BufferedReader;
+import java.io.StringReader;
+import java.math.BigInteger;
+import java.util.ArrayList;
+
+//В классе Solution замени все короткие имена классов в коде на полные.
+//
+//Требования:
+//        •	В методе main должны быть только полные имена классов:
+//остальное менять нельзя.
+//•	В классе Solution не должно быть импортов.
+
+public class Main {
+    public static void main(String[] args) {
+        java.util.ArrayList<String> arrayList = new java.util.ArrayList<>();
+        java.io.BufferedReader bufferedReader = new java.io.BufferedReader(new java.io.StringReader(""));
+        java.math.BigInteger bigInteger = new java.math.BigInteger("123");
+    }
+}
---
+++ Core/Math/Task0807/Main.java
@@ -0,0 +1,13 @@
+package Math.Task0807;
+
+import java.io.InputStreamReader;
+import java.util.LinkedList;
+import java.util.concurrent.atomic.AtomicBoolean;
+
+public class Main {
+    public static void main(String[] args) {
+        LinkedList<String> linkedList = new LinkedList<>();
+        InputStreamReader inputStreamReader = new InputStreamReader(System.in);
+        AtomicBoolean atomicBoolean = new AtomicBoolean(false);
+    }
+}
