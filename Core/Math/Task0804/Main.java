f1dc3a314c9cb3ef607e7e7890d93695c3c64765
Yuriy Perezhogin <wolfviking086@gmail.com>
2025-05-10 09:43:38 +0500

    update Task0804

--- Core/Math/Task0804/Main.java
+++ Core/Math/Task0804/Main.java
@@ -9,13 +9,16 @@ package Math.Task0804;
 //с типом возвращаемого значения int.
 //        •	Реализуй метод generateNumber() согласно условию.
 
+import java.util.Random;
+
 public class Main {
     public static void main(String[] args) {
         System.out.println(generateNumber());
     }
 
     public static int generateNumber() {
+        int random = new Random().nextInt(100);
 
-        return  (int) (Math.random() * 100);
+        return  random;
     }
 }
