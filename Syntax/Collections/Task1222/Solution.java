4dcd8613693e237681b57cd136d768791d1f8482
wolfviking1979 <wolfviking@yandex.ru>
2025-05-15 13:15:29 +0500

    add Task1215, Task1220, Task1221,Task1222

---
+++ Syntax/Collections/Task1222/Solution.java
@@ -0,0 +1,21 @@
+package Collections.Task1222;
+
+import java.util.ArrayList;
+
+public class Solution {
+    public static void printAnything(ArrayList arrayList) {
+        for (int i = 0; i < arrayList.size(); i++) {
+            System.out.println(arrayList.get(i));
+        }
+    }
+
+    public static void main(String[] args) {
+        var arrayList = new ArrayList<>();
+        arrayList.add(15);
+        arrayList.add("Hello");
+        arrayList.add(154);
+        arrayList.add("string");
+
+        printAnything(arrayList);
+    }
+}
