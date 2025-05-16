e04ac4358e823d4367745bb497469a6c7405b186
wolfviking1979 <wolfviking@yandex.ru>
2025-05-16 18:47:13 +0500

    add Task1402, Task1412,

---
+++ Syntax/Exception/Task1412/Solution.java
@@ -0,0 +1,17 @@
+package Exception.Task1412;
+
+public class Solution {
+    public static void main(String[] args) {
+        try {
+            System.out.println("Программа работает от забора");
+            Thread.sleep(1000);
+            System.out.println("до обеда");
+        } catch (NullPointerException | NumberFormatException e) {
+            System.out.println("Произошло исключение на букву N");
+        } catch (IllegalArgumentException | IllegalStateException | InterruptedException e) {
+            System.out.println("Произошло исключение на букву I");
+        } catch (ClassCastException e) {
+            System.out.println("Произошло исключение на букву C");
+        }
+    }
+}
